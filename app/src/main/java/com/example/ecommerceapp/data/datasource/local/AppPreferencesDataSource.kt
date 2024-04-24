0bdaa9690d05fd107a283bc68c35a012d9809096
Zyad Mohamed <zyadmuhammed05@gmail.com>
2024-04-24 22:35:18 +0200

    create AppPreferencesDataSource.kt

---
+++ app/src/main/java/com/example/ecommerceapp/data/datasource/local/AppPreferencesDataSource.kt
@@ -0,0 +1,19 @@
+package com.example.ecommerceapp.data.datasource.local
+
+import android.content.Context
+import androidx.datastore.preferences.core.edit
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+class AppPreferencesDataSource(private val context: Context) {
+
+    suspend fun saveLoginState(isLoggedIn: Boolean) {
+        context.appDataStore.edit { preferences ->
+            preferences[DataStoreKeys.IS_USER_LOGGED_IN] = isLoggedIn
+        }
+    }
+    val isUserLoggedIn: Flow<Boolean> = context.appDataStore.data.map { preferences ->
+        preferences[DataStoreKeys.IS_USER_LOGGED_IN] ?: false
+    }
+
+}
